b3765233edadcde35a046631839da1f5b73d8846
Lucca Fagundes <luccapfagundes@gmail.com>
2026-04-22 21:28:11 -0300

    Add files via upload

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+
+// Componente simples de cabeçalho
+export default function Header() {
+  return (
+    <header style={{ textAlign: "center", marginBottom: "20px" }}>
+      <h1>🚀 NASA - Signo dos nerds </h1>
+      <p>Escolha a data que representa seu "signo" espacial!</p>
+    </header>
+  );
+}
---
+++ src/components/LinkGithub.tsx
@@ -0,0 +1,9 @@
+export default function LinkGithub() {
+    return (
+      <footer style={{ textAlign: "center", marginBottom: "20px" }}>
+        <h1>As melhores APIs e códigos de Riozinho</h1>
+        <a href="https://github.com/Gabiss1">Pegue seus códigos aqui</a>
+      </footer>
+    );
+  }
+  
